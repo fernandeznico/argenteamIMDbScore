a4a1865314bff9953cf13989d6d566de48573103
Mariano Desanze <protronm@gmail.com>
2022-08-28 01:05:14 -0300

    ICON_BASE64

--- argenteamIMDBscore.js
+++ argenteamIMDBscore.js
@@ -1,6 +1,6 @@
 var moviesInfo = document.querySelectorAll('.movie-info');
 
-var ICON_URL = "https://www.flaticon.es/svg/static/icons/svg/889/889118.svg"
+const ICON_BASE64 = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAABZklEQVQ4T52SP0vDUBTFz0v6T0QjVqstaqUq2NZFxVFRRNBNEPwSDn4BnZz8Ag5F144uil0cpA4iDsWh+A+xFsQMFixCaSTm+R43hUiC0PzgcO577+YkeVxm3K3xn88C/KD2rII1rsMclmFvtQkLQPH9sISbYPo5+E0ZGI0Dt09AVKOzWh3IjgHlZ6AzAqRTQKwXyJ8B8zPAxAj1oXgELozvb5NnUiRZ722RS0VC4BeHVOd2wRtXJEVs/EGvkZxsrgDNb6B0T2vTBKo61a6A+hdJdZwkxe9JDBEi2TkAMhvilx89ArxgjNyyyNeXqH558wjQukj/kegnt8SNuAJm0yQnslHS+hInroB4H8nJQ4U8HCJv2qMTDHgEDEZJTk6K5K3LzB2TyzWrFsBPRcNcVrzpFZgap8OKuKBkggZJMhQDpieByxIwPAC8fwALYqCYHAZq8YcCpcMufcCCUNTuRXvVPqq2jF+8329wYlZVeQAAAABJRU5ErkJggg=='
 
 for(let pos in moviesInfo){
     if(moviesInfo[pos].innerText === undefined) continue;
@@ -22,7 +22,7 @@ for(let pos in moviesInfo){
             var imdbDataA = document.createElement('a');
             imdbDataA.href = 'https://www.imdb.com/title/' + data['imdbID']
             imdbDataA.innerHTML = imdbDataText
-            imdbDataA.style.background = 'url(' + ICON_URL + ')';
+            imdbDataA.style.backgroundImage = 'url(data:image/png;base64,' + ICON_BASE64 + ')';
             imdbDataA.style.backgroundRepeat = 'no-repeat';
             imdbDataA.style.backgroundSize = '16px 16px';
             imdbDataA.style.paddingTop = '0px';
